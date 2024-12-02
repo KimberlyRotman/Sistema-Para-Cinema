e828152c1f288bff189fb5ac5867b2033ac64422
Maipu <135667376+KimberlyRotman@users.noreply.github.com>
2024-12-02 13:34:35 -0300

    Organizando arquivos

---
+++ Código/JAVA/Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente{
+    
+    Int cpf; //id
+    String nome;
+    Int idade;
+    String tipo; //se é cadeirante por exemplo ou sucetível a desconto
+    List contato; // TODO: invés de ser um int telefone, transformar em uma List para ser possível uma pessoa ter mais de um telefone
+    String email;
+    Lista objeto Sessao //TODO: instanciar corretamente o objeto sessao pq o cliente pode estar em mais de uma sessao
+
+    
+}
---
+++ Código/JAVA/ClienteCadastro.java
@@ -0,0 +1,7 @@
+public class ClienteCadastro{
+    /*
+    TODO:
+    método de cadastrar cliente via terminal
+    método de colocar o cliente em uma lista de clientes
+    */
+}
